8d9d48c02e0ab0b294f15e6f11f1edd391de6f61
Kennethyeon1 <600020446@fjuhsd.org>
2021-04-27 16:31:38 +0000

    completed question 1

--- Unit8_1b.java
+++ Unit8_1b.java
@@ -1,8 +1,18 @@
-public class Unit8_1a
+public class Unit8_1b
 {
   public int [] [] modifyRows (int [] [] array)
- { 
-return array;
+  {
+    for (int r = 0; r < array.length; r++)
+    {
+      for (int c = 0; c < array[0].length; c++)
+      {
+          if (r%2 != 0)
+          {
+            array[r][c] = array[r - 1][c];
+          }
       }
     }
+    return array;
+  }
+}
  
